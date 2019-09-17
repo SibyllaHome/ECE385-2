b6996dd6bd1c1867227207ec9b73933332dde713
xueytian <32151926+xueytian@users.noreply.github.com>
2019-09-16 22:01:55 -0500

    adder1.1

--- Lab4/Lab_4_adders/carry_lookahead_adder.sv
+++ Lab4/Lab_4_adders/carry_lookahead_adder.sv
@@ -13,12 +13,17 @@ module carry_lookahead_adder
      * Feel free to create sub-modules or other files. */
    logic [3:0] p;
 	logic [3:0] g;
-	logic [3:0] c;
-	logic i;
+	logic [4:0] c;
+	//logic i;
+	
+	
 	always_comb
 	begin
-		
-		for (i=1; i<5; i=i+1) begin
+			//c[1] = (0 & p[0]) | g[0]; 
+			//c[2] = (0 & p[0] & p[1]) | (g[0] & p[1]) | g[1];
+			//c[3] = (0 & p[0] & p[1] & p[2]) | (g[0] & p[1] & p[2]) | (g[1] & p[2]) | g[2];
+			//CO   = (0 & p[0] & p[1] & p[2] & p[3]) | (g[0] & p[1] & p[2] & p[3]) | (g[1] & p[2] & p[3]) | (g[2] & p[3]) | g[3];
+		for (int i=1; i<5; i=i+1) begin
 			c[0] = 0;
 			c[i] = g[i-1] | (p[i-1] & c[i-1]);
 		end
@@ -26,7 +31,7 @@ module carry_lookahead_adder
 		CO = c[4];
 	end
 	
-	four_bit_cla cla0(.x(A[3 : 0]), .y(B[3 : 0]), .cin(   0), .s(Sum[3 : 0]), .pg(p[0]), .gg(g[0]));
+	four_bit_cla cla0(.x(A[3 : 0]), .y(B[3 : 0]), .cin(1'b0), .s(Sum[3 : 0]), .pg(p[0]), .gg(g[0]));
 	four_bit_cla cla1(.x(A[7 : 4]), .y(B[7 : 4]), .cin(c[1]), .s(Sum[7 : 4]), .pg(p[1]), .gg(g[1]));
 	four_bit_cla cla2(.x(A[11: 8]), .y(B[11: 8]), .cin(c[2]), .s(Sum[11: 8]), .pg(p[2]), .gg(g[2]));
 	four_bit_cla cla3(.x(A[15:12]), .y(B[15:12]), .cin(c[3]), .s(Sum[15:12]), .pg(p[3]), .gg(g[3]));
@@ -47,19 +52,29 @@ module four_bit_cla(
 	logic [3:0] g;
 	logic [3:0] c;
 	logic i;
+	
+	assign p = x | y;
+	assign g = x & y;
+	
 	always_comb
 	begin
 	
-		for (i=0; i<4; i=i+1) begin
-			g[i] = x[i] & y[i];
-			p[i] = x[i] ^ y[i];
-	 
-		end
-		
-		for (i=1; i<4; i=i+1) begin
-			c[0] = cin;
-			c[i] = g[i-1] | (p[i-1] & c[i-1]);
-		end
+		c[1] = (1'b0 & p[0]) | g[0]; 
+		c[2] = (1'b0 & p[0] & p[1]) | (g[0] & p[1]) | g[1];
+		c[3] = (1'b0 & p[0] & p[1] & p[2]) | (g[0] & p[1] & p[2]) | (g[1] & p[2]) | g[2];
+		pg = (p[0] & p[1] & p[2] & p[3]);
+		gg = (g[0] & p[1] & p[2] & p[3]) | (g[1] & p[2] & p[3]) | (g[2] & p[3]) | g[3];
+	
+		//for (i=0; i<4; i=i+1) begin
+		//	g[i] = x[i] & y[i];
+		//	p[i] = x[i] ^ y[i];
+	 //
+		//end
+		//
+		//for (i=1; i<4; i=i+1) begin
+		//	c[0] = cin;
+		//	c[i] = g[i-1] | (p[i-1] & c[i-1]);
+		//end
 	
 	end
 	

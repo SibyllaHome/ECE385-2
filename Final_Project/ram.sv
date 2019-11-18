cd8f57f905e62c9343f307546d2dcfdfbd63644d
Zongnan Bao <43384186+bznick98@users.noreply.github.com>
2019-11-17 21:16:18 -0600

    Add test Ram Small

--- Final_Project/ram.sv
+++ Final_Project/ram.sv
@@ -15,3 +15,22 @@ begin
 end
 
 endmodule
+
+
+module testRamSmall (
+					 input [9:0] read_addr,		// 2 ^ 10
+					 input clk,
+					 output logic [7:0] data_out);
+
+logic [7:0] mem [0:791]; // 792 lines
+
+initial begin
+	$readmemh("Right.txt",mem);
+end
+
+always_ff @ (posedge clk)
+begin
+	data_out <= mem[read_addr];
+end
+
+endmodule
